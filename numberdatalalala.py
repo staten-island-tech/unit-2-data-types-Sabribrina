97210d5c9d7debd2d636899cd2287fa4c595aab1
Sabribrina <sabrinal117@nycstudents.net>
2026-02-03 10:16:20 -0500

    2/3/26

--- numberdatalalala.py
+++ numberdatalalala.py
@@ -19,11 +19,49 @@ z = y[0]
 print(y)
 print(z) """
 
-sentence = input("input a sentence")
-
-
 """ day_of_week = input("what day is it?")
 if day_of_week == "Friday":
     print("correct")
 else:
-    print("incorrect") """
+    print("incorrect") """ 
+""" 
+x = "test"
+print(f"hello {x}") """
+""" 
+temp = 75
+if temp > 68:
+    print('warm')
+elif temp == 68:
+    print('perfect')
+else:
+    print('cold') """
+
+sentence = input("input a sentence")
+
+
+
+""" number = int(input("give me number"))
+
+if number >= 21:
+    print("can vote and gamble")
+elif(number >= 18):
+    print("Can vote but not gamble")
+else:
+    print("have a juicebox kid")
+
+temp = 55
+if (temp > 40 and temp < 60):
+    print("lightjacket need")
+elif (temp < 20 or temp > 90):
+    print("extreme") """
+
+""" age = int(input("give age"))
+def discount(isMember, age, isResident):
+    if (isMember or isResident):
+        print("discount applies")
+    if age >=65:
+        print("discount applies")
+    if age <=12:
+        print("discount applies")
+    else:
+        print("discount does not apply") #uhohs??? """
